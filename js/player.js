e2cd4d0e7e9f59df624122db8937b7f65b762cd7
Nat Welch <nat@natwelch.com>
2012-09-29 10:21:04 -0700

    Put magic numbers in some variables.

--- js/player.js
+++ js/player.js
@@ -32,9 +32,12 @@ function loadPixelPica(x, y) {
    var height = 16;
    var width = 16;
 
+   var jumpSpeed = 2;
+   var runSpeed = 5;
+
    var pica = Crafty.e("2D, Canvas, pixel-pica").attr({'w': width, 'h': height, 'x': x, 'y': y});
 
-   pica.addComponent("Twoway").twoway(5, 2)
+   pica.addComponent("Twoway").twoway(runSpeed, jumpSpeed);
 
    pica.addComponent('SpriteAnimation').animate('pixel-blink', 0, 0, 3).animate('pixel-blink', 30, -1);
 
@@ -42,9 +45,12 @@ function loadPixelPica(x, y) {
 }
 
 function loadLegPica(x, y) {
+   var jumpSpeed = 7;
+   var runSpeed = 5;
+
    var pica = Crafty.e("2D, Canvas, leg-pica").attr({'w': 32, 'h': 32, 'x': x, 'y': y});
 
-   pica.addComponent("Twoway").twoway(5, 5)
+   pica.addComponent("Twoway").twoway(runSpeed, jumpSpeed);
 
    pica.addComponent('SpriteAnimation').animate('leg-run', 0, 1, 3).animate('leg-run', 30, -1);
 
