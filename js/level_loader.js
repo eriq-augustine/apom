4bc14e62f34395dd4e09c96625e26da216c1e996
Christopher Hebert <hebert.christopherj@gmail.com>
2012-09-30 14:36:13 -0700

    added a start/end to env.png

--- js/level_loader.js
+++ js/level_loader.js
@@ -1,5 +1,3 @@
-var test_level = {"rows":12,"cols":16,"tile":{"width":32,"height":32},"map":{"Platform":[{"row":2,"col":5}],"Vine":[{"row":2,"col":6},{"row":3,"col":5},{"row":3,"col":6},{"row":3,"col":7},{"row":4,"col":6},{"row":4,"col":7}]}};
-
 function LoadLevel(level) {
    for (var key in level.map) {
       var elements = level.map[key];
@@ -9,5 +7,3 @@ function LoadLevel(level) {
       }
    }
 };
-
-LoadLevel(test_level);
